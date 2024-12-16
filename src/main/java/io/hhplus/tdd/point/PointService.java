cce10230845dcce6e5002be17b90f2aa64005098
Hyejin <hyejin.jeon940120@gmail.com>
2024-12-16 17:36:20 +0900

    init commit

---
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -0,0 +1,11 @@
+package io.hhplus.tdd.point;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class PointService {
+}
