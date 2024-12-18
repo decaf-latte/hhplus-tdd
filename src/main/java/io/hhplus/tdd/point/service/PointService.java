98771f8e1c26c1c34788b6574889cdc7cd26fc38
Hyejin <hyejin.jeon940120@gmail.com>
2024-12-18 17:50:22 +0900

    feat : 최대 포인트 변수 추가, 예외 처리 로그 수정

--- src/main/java/io/hhplus/tdd/point/service/PointService.java
+++ src/main/java/io/hhplus/tdd/point/service/PointService.java
@@ -20,6 +20,7 @@ public class PointService {
 
     private final PointHistoryTable pointHistoryTable;
     private final UserPointTable userPointTable;
+    private static final long MAX_BALANCE = 1_000_000L;//최대 포인트 예시
 
     //특정 유저의 포인트를 조회하는 기능
     public UserPoint getUserPointByUserId(long userId) {
@@ -38,20 +39,17 @@ public class PointService {
     //특정 유저의 포인트를 충전하는 기능
     public UserPoint chargeUserPoint(long id, long amount) {
 
+        //충전 금액 0, 음수 예외처리
         if (amount <= 0) {
             throw new PointException(HttpStatus.BAD_REQUEST, "충전 금액은 0보다 커야 합니다.");
         }
 
         UserPoint userPoint = userPointTable.selectById(id);
-        if (userPoint == null) {
-            throw new PointException(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다.");
-        }
-
         long updatedPoint = userPoint.point() + amount;
 
         // 최대 잔고 초과 예외 처리
-        if (updatedPoint < 0) { // Long overflow 방지
-            throw new PointException(HttpStatus.BAD_REQUEST, "최대 잔고를 초과할 수 없습니다.");
+        if (updatedPoint < MAX_BALANCE) {
+            throw new PointException(HttpStatus.BAD_REQUEST, "최대 잔고는 " + MAX_BALANCE + "을 초과할 수 없습니다.");
         }
 
         // 포인트 히스토리 추가
@@ -62,17 +60,18 @@ public class PointService {
 
     //특정 유저의 포인트를 사용하는 기능을 작성
     public UserPoint usePoint(long id, long amount) {
+        //사용금액 0,음수 예외처리
         if (amount <= 0) {
             throw new PointException(HttpStatus.BAD_REQUEST, "사용 금액은 0보다 커야 합니다.");
         }
 
         UserPoint userPoint = userPointTable.selectById(id);
-        if (userPoint == null) {
-            throw new PointException(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다.");
-        }
 
+        //포인트 잔고부족 예외처리
         if (userPoint.point() < amount) {
-            throw new PointException(HttpStatus.BAD_REQUEST, "포인트가 부족합니다.");
+            throw new PointException(HttpStatus.BAD_REQUEST,
+                    "포인트가 부족합니다. 현재 잔액: " + userPoint.point() + "원, 요청 금액: " + amount + "원");
+
         }
 
         // 포인트 히스토리 추가
